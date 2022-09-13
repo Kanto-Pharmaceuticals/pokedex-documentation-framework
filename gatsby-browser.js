86378fbf8a5ffd63f66e73f98fa09bf2d37e886b
mattybakes <mattybakes@users.noreply.github.com>
2022-09-13 22:00:48 +0000

    Prettified Code!

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,8 +3,8 @@ import "typeface-montserrat"
 import "typeface-merriweather"
 
 /* Begin CSS imports, ordering is important. */
-import "./src/normalize.css"                    // normalize all browsers 
-import "./src/palette.css"                      // palette colours 
-import "./src/typography.css"                   // @font-face definitions
-import "./src/style.css"                        // global styles and utility
-import "prismjs/themes/prism.css"               // prismjs syntax highlighting
+import "./src/normalize.css" // normalize all browsers
+import "./src/palette.css" // palette colours
+import "./src/typography.css" // @font-face definitions
+import "./src/style.css" // global styles and utility
+import "prismjs/themes/prism.css" // prismjs syntax highlighting
