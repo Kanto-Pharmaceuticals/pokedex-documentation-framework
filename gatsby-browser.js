4ebcbb8fd340629b94513e29881e994ff3750f6e
mattybakes@coconut <mpoon2@athabasca.edu>
2022-09-13 17:59:59 -0400

    css templating

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,10 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
-// normalize CSS across browsers
-import "./src/normalize.css"
-// custom CSS styles
-import "./src/style.css"
 
-// Highlighting for code blocks
-import "prismjs/themes/prism.css"
+/* Begin CSS imports, ordering is important. */
+import "./src/normalize.css"                    // normalize all browsers 
+import "./src/palette.css"                      // palette colours 
+import "./src/typography.css"                   // @font-face definitions
+import "./src/style.css"                        // global styles and utility
+import "prismjs/themes/prism.css"               // prismjs syntax highlighting
