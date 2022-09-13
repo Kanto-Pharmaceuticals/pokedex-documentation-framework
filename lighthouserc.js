6531c646a035658fdc73017fbf279f517428c5fa
mattybakes <mattybakes@users.noreply.github.com>
2022-09-13 17:25:33 +0000

    Prettified Code!

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,38 +1,38 @@
 module.exports = {
-    ci: {
-      collect: {
-        numberOfRuns: 5,
-        startServerCommand: 'yarn serve',
-        url: ['http://localhost:4222/'],
-        settings: {
-          skipAudits: ['uses-http2'],
-          chromeFlags: '--no-sandbox',
-        },
+  ci: {
+    collect: {
+      numberOfRuns: 5,
+      startServerCommand: "yarn serve",
+      url: ["http://localhost:4222/"],
+      settings: {
+        skipAudits: ["uses-http2"],
+        chromeFlags: "--no-sandbox",
       },
-      assert: {
-        assertions: {
-          'categories:performance': [
-            'error',
-            { minScore: 0.9, aggregationMethod: 'median-run' },
-          ],
-          'categories:accessibility': [
-            'error',
-            { minScore: 1, aggregationMethod: 'pessimistic' },
-          ],
-          'categories:best-practices': [
-            'error',
-            { minScore: 1, aggregationMethod: 'pessimistic' },
-          ],
-          'categories:seo': [
-            'error',
-            { minScore: 1, aggregationMethod: 'pessimistic' },
-          ],
-        },
-      },
-      upload: {
-        target: 'lhci',
-        token: 'f882df13-b3ed-4781-82fc-e22ace400523',
-        serverBaseUrl: "https://lhci.mattycakes.ca"
-      }
     },
-  }
+    assert: {
+      assertions: {
+        "categories:performance": [
+          "error",
+          { minScore: 0.9, aggregationMethod: "median-run" },
+        ],
+        "categories:accessibility": [
+          "error",
+          { minScore: 1, aggregationMethod: "pessimistic" },
+        ],
+        "categories:best-practices": [
+          "error",
+          { minScore: 1, aggregationMethod: "pessimistic" },
+        ],
+        "categories:seo": [
+          "error",
+          { minScore: 1, aggregationMethod: "pessimistic" },
+        ],
+      },
+    },
+    upload: {
+      target: "lhci",
+      token: "f882df13-b3ed-4781-82fc-e22ace400523",
+      serverBaseUrl: "https://lhci.mattycakes.ca",
+    },
+  },
+}
